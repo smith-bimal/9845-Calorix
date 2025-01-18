d36f0f71514c2c3b512cf73daf02568dda14b1ab
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-18 10:56:51 +0530

    Completed dish and item routes

--- server/routers/dish.route.js
+++ server/routers/dish.route.js
@@ -1,38 +1,10 @@
 const express = require("express");
-const Dish = require("../models/dish");
+const { addDish, getDish, deleteDish, updateDish } = require("../controllers/dish.controller");
 const router = express.Router();
 
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("hello dishes");
-})
+router.post("/", addDish);
 
-router.route('/:id').get(async (req, res) => {
-    try {
-        const dish = await Dish.findById(req.params.id);
-        res.status(200).send(dish);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-})
-    .put(async (req, res) => {
-        try {
-            await Dish.findByIdAndUpdate(req.params.id, {
-                name: req.body.name,
-                items: [
-
-                ]
-            })
-        } catch (e) {
-
-        }
-    }).delete(async (req, res) => {
-        try {
-            await Dish.findByIdAndDelete(req.params.id);
-            res.status(200).json({ message: "Dish deleted successfully." });
-        } catch (e) {
-            res.status(500).json({ message: e.message });
-        }
-    })
+router.route('/:id').get(getDish)
+    .put(updateDish).delete(deleteDish)
 
 module.exports = router;
