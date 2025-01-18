9a6ffa6098858986be20c05c10a8af4f0580921e
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-18 18:26:10 +0530

    Completed QR functionality and completed the landing page

---
+++ client/src/components/QRScanner.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import { useNavigate } from "react-router";
+
+function QRScanner() {
+    const [data, setData] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log(data);
+        if (data != '') navigate("/dishes");
+        // navigate(data);
+    }, [data]);
+
+    return (
+        <div className="w-[400px] absolute left-1/2 -translate-x-1/2">
+            <BarcodeScannerComponent
+                width={500}
+                height={500}
+                onUpdate={(err, result) => {
+                    if (result) {
+                        setData(result.text);
+                    }
+                }}
+            />
+        </div>
+    );
+}
+
+export default QRScanner;
