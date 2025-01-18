2ec242cdfa36a353e638f474756f0fbb3fa298c4
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-18 12:37:00 +0530

    Updated cors options

--- server/index.js
+++ server/index.js
@@ -17,7 +17,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
+
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
