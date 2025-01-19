908ae2c9cccf17a876942bb8ad87a21c9d57a1ae
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-19 09:56:44 +0530

    Finished all counter and dynamic calories change process.

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -1,5 +1,20 @@
 /* eslint-disable react/prop-types */
-const Counter = ({ dish }) => {
+const Counter = ({ dish, quantities, setQuantities }) => {
+    const handleDecrease = () => {
+        if (quantities[dish._id] > 0) {
+            setQuantities((prev) => ({
+                ...prev,
+                [dish._id]: prev[dish._id] - 1,
+            }));
+        }
+    };
+
+    const handleIncrease = () => {
+        setQuantities((prev) => ({
+            ...prev,
+            [dish._id]: (prev[dish._id] || 0) + 1,
+        }));
+    };
 
     return (
         <div className="w-fit flex items-center gap-6">
@@ -9,12 +24,12 @@ const Counter = ({ dish }) => {
             </div>
 
             <div className="bg-white px-6 py-2 w-40 rounded-full shadow-[0_4px_4px_0_#0000001f,_inset_1px_0_4px_#0000001a] flex items-center justify-between text-2xl">
-                <button onClick={() => { }} className="outline-none">-</button>
-                <p>{dish.quantity}</p>
-                <button onClick={() => { }} className="outline-none">+</button>
+                <button onClick={handleDecrease} className="outline-none">-</button>
+                <p>{quantities[dish._id] || 0}</p>
+                <button onClick={handleIncrease} className="outline-none">+</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Counter
+export default Counter;
