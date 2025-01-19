35e70c3b4e124d830d389841485606c62a422a77
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-19 18:46:44 +0530

    Updated Client URL

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://calorix.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
