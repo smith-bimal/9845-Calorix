00a366dee4cd7a28d8794195b2db38eb3234a468
smith-bimal <golokbihareebeemal@gmail.com>
2025-01-19 18:42:25 +0530

    Updated Server URL

--- client/src/lib/authService.js
+++ client/src/lib/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api/auth";
+const API_BASE_URL = "https://nine845-calorix.onrender.com/api/auth";
 
 export const registerUser = async (data) => {
     return axios.post(`${API_BASE_URL}/register`, data);
